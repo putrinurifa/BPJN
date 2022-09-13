a2bdea697bf8791dd06c8cf850703a5b06b0fe6e
putrinurifa <putrinurifa818@gmail.com>
2022-09-13 12:07:14 +0700

    organisasi

--- resources/views/BPJN/organisasi.blade.php
+++ resources/views/BPJN/organisasi.blade.php
@@ -11,6 +11,12 @@
             <h2>Struktur Organisasi</h2>
         </div>
 
+        <div class="row justify-content-center" data-aos="fade-up">
+
+            <div class="me-md-auto text-center text-md-start ">
+                <img src="{{ asset('assets') }}/img/organisasi.png" height="100%" width="100%" />
+            </div>
+        </div>
     </div>
 </section><!-- End Contact Section -->
 @endsection
