ad09178b3eb993dee2bbbfaaadc3a32b08e97afd
putrinurifa <putrinurifa818@gmail.com>
2022-12-15 14:36:58 +0700

    layanan

--- resources/views/BPJN/kelahiran.blade.php
+++ resources/views/BPJN/kelahiran.blade.php
@@ -6,146 +6,88 @@
 <script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
 <!------ Include the above in your HEAD tag ---------->
 <br><br><br><br><br><br><br><br>
-    <div class="container">    
-        <div id="loginbox" style="margin-top:50px;" class="mainbox col-md-6 col-md-offset-3 col-sm-8 col-sm-offset-2">                    
-            <div class="panel panel-info" >
-                    <div class="panel-heading">
-                        <div class="panel-title">Sign In</div>
-                        <div style="float:right; font-size: 80%; position: relative; top:-10px"><a href="#">Forgot password?</a></div>
-                    </div>     
+<div class="container">
+    <div id="signupbox" style="margin-top:50px"
+        class="mainbox col-md-6 col-md-offset-3 col-sm-8 col-sm-offset-2">
+        <div class="panel panel-info">
+            <div class="panel-heading">
+                <div class="panel-title">Formulir Surat Kelahiran</div>
+            </div>
+            <div class="panel-body">
+                <form id="signupform" class="form-horizontal" role="form">
 
-                    <div style="padding-top:30px" class="panel-body" >
-
-                        <div style="display:none" id="login-alert" class="alert alert-danger col-sm-12"></div>
-                            
-                        <form id="loginform" class="form-horizontal" role="form">
-                                    
-                            <div style="margin-bottom: 25px" class="input-group">
-                                        <span class="input-group-addon"><i class="glyphicon glyphicon-user"></i></span>
-                                        <input id="login-username" type="text" class="form-control" name="username" value="" placeholder="username or email">                                        
-                                    </div>
-                                
-                            <div style="margin-bottom: 25px" class="input-group">
-                                        <span class="input-group-addon"><i class="glyphicon glyphicon-lock"></i></span>
-                                        <input id="login-password" type="password" class="form-control" name="password" placeholder="password">
-                                    </div>
-                                    
-
-                                
-                            <div class="input-group">
-                                      <div class="checkbox">
-                                        <label>
-                                          <input id="login-remember" type="checkbox" name="remember" value="1"> Remember me
-                                        </label>
-                                      </div>
-                                    </div>
-
-
-                                <div style="margin-top:10px" class="form-group">
-                                    <!-- Button -->
-
-                                    <div class="col-sm-12 controls">
-                                      <a id="btn-login" href="#" class="btn btn-success">Login  </a>
-                                      <a id="btn-fblogin" href="#" class="btn btn-primary">Login with Facebook</a>
-
-                                    </div>
-                                </div>
-
-
-                                <div class="form-group">
-                                    <div class="col-md-12 control">
-                                        <div style="border-top: 1px solid#888; padding-top:15px; font-size:85%" >
-                                            Don't have an account! 
-                                        <a href="#" onClick="$('#loginbox').hide(); $('#signupbox').show()">
-                                            Sign Up Here
-                                        </a>
-                                        </div>
-                                    </div>
-                                </div>    
-                            </form>     
-
-
-
-                        </div>                     
-                    </div>  
-        </div>
-        <div id="signupbox" style="display:none; margin-top:50px" class="mainbox col-md-6 col-md-offset-3 col-sm-8 col-sm-offset-2">
-                    <div class="panel panel-info">
-                        <div class="panel-heading">
-                            <div class="panel-title">Sign Up</div>
-                            <div style="float:right; font-size: 85%; position: relative; top:-10px"><a id="signinlink" href="#" onclick="$('#signupbox').hide(); $('#loginbox').show()">Sign In</a></div>
-                        </div>  
-                        <div class="panel-body" >
-                            <form id="signupform" class="form-horizontal" role="form">
-                                
-                                <div id="signupalert" style="display:none" class="alert alert-danger">
-                                    <p>Error:</p>
-                                    <span></span>
-                                </div>
-                                    
-                                
-                                  
-                                <div class="form-group">
-                                    <label for="email" class="col-md-3 control-label">Email</label>
-                                    <div class="col-md-9">
-                                        <input type="text" class="form-control" name="email" placeholder="Email Address">
-                                    </div>
-                                </div>
-                                    
-                                <div class="form-group">
-                                    <label for="firstname" class="col-md-3 control-label">First Name</label>
-                                    <div class="col-md-9">
-                                        <input type="text" class="form-control" name="firstname" placeholder="First Name">
-                                    </div>
-                                </div>
-                                <div class="form-group">
-                                    <label for="lastname" class="col-md-3 control-label">Last Name</label>
-                                    <div class="col-md-9">
-                                        <input type="text" class="form-control" name="lastname" placeholder="Last Name">
-                                    </div>
-                                </div>
-                                <div class="form-group">
-                                    <label for="password" class="col-md-3 control-label">Password</label>
-                                    <div class="col-md-9">
-                                        <input type="password" class="form-control" name="passwd" placeholder="Password">
-                                    </div>
-                                </div>
-                                    
-                                <div class="form-group">
-                                    <label for="icode" class="col-md-3 control-label">Invitation Code</label>
-                                    <div class="col-md-9">
-                                        <input type="text" class="form-control" name="icode" placeholder="">
-                                    </div>
-                                </div>
-
-                                <div class="form-group">
-                                    <!-- Button -->                                        
-                                    <div class="col-md-offset-3 col-md-9">
-                                        <button id="btn-signup" type="button" class="btn btn-info"><i class="icon-hand-right"></i> &nbsp Sign Up</button>
-                                        <span style="margin-left:8px;">or</span>  
-                                    </div>
-                                </div>
-                                
-                                <div style="border-top: 1px solid #999; padding-top:20px"  class="form-group">
-                                    
-                                    <div class="col-md-offset-3 col-md-9">
-                                        <button id="btn-fbsignup" type="button" class="btn btn-primary"><i class="icon-facebook"></i>   Sign Up with Facebook</button>
-                                    </div>                                           
-                                        
-                                </div>
-                                
-                                
-                                
-                            </form>
-                         </div>
+                    <div id="signupalert" style="display:none" class="alert alert-danger">
+                        <p>Error:</p>
+                        <span></span>
                     </div>
 
-               
-               
-                
-         </div> 
+
+
+                    <div class="form-group">
+                        <label for="nama_anak" class="col-md-3 control-label">Nama Anak</label>
+                        <div class="col-md-9">
+                            <input type="text" class="form-control" name="nama_anak" placeholder="Nama">
+                        </div>
+                    </div>
+
+                    <div class="form-group">
+                        <label for="tgl_lhr" class="col-md-3 control-label">Tempat/Tanggal Lahir</label>
+                        <div class="col-md-9">
+                            <input type="text" class="form-control" name="tgl_lhr" placeholder="Tempat/Tanggal Lahir">
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <label for="jk" class="col-md-3 control-label">Jenis Kelamin</label>
+                        <div class="col-md-9">
+                            <input type="text" class="form-control" name="jk" placeholder="Jenis Kelamin">
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <label for="alamat" class="col-md-3 control-label">Alamat</label>
+                        <div class="col-md-9">
+                            <input type="alamat" class="form-control" name="alamat" placeholder="Alamat">
+                        </div>
+                    </div>
+
+                    
+                    <div class="border mt-3 border-dashed"></div><br>
+
+                    <div class="form-group">
+                        <label for="nama_ayah" class="col-md-3 control-label">Nama Ayah</label>
+                        <div class="col-md-9">
+                            <input type="nama_ayah" class="form-control" name="nama_ayah" placeholder="Nama Ayah">
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <label for="nama_ibu" class="col-md-3 control-label">Nama Ibu</label>
+                        <div class="col-md-9">
+                            <input type="nama_ibu" class="form-control" name="nama_ibu" placeholder="Nama Ibu">
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <label for="anakke" class="col-md-3 control-label">Anak Ke</label>
+                        <div class="col-md-9">
+                            <input type="anakke" class="form-control" name="anakke" placeholder="Anak Ke">
+                        </div>
+                    </div>
+
+                    <div style="float:right" class="form-group">
+                        <!-- Button -->
+
+                        <div class="col-sm-12 controls">
+                            <a id="btn-fblogin" href="#" class="btn btn-primary">Kirim</a>
+
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </div>
+
+
+
+
     </div>
-    <br><br><br><br><br><br>
-    
+</div>
+<br><br>
+
 @endsection
-   
---
+++ resources/views/BPJN/kematian.blade.php
@@ -0,0 +1,89 @@
+@extends('layouts.layanan')
+
+@section('content')
+<link href="//netdna.bootstrapcdn.com/bootstrap/3.1.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+<script src="//netdna.bootstrapcdn.com/bootstrap/3.1.0/js/bootstrap.min.js"></script>
+<script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
+<!------ Include the above in your HEAD tag ---------->
+<br><br><br><br><br><br><br><br>
+<div class="container">
+    <div id="signupbox" style="margin-top:50px"
+        class="mainbox col-md-6 col-md-offset-3 col-sm-8 col-sm-offset-2">
+        <div class="panel panel-info">
+            <div class="panel-heading">
+                <div class="panel-title">Formulir Surat Keematian</div>
+            </div>
+            <div class="panel-body">
+                <form id="signupform" class="form-horizontal" role="form">
+
+                    <div id="signupalert" style="display:none" class="alert alert-danger">
+                        <p>Error:</p>
+                        <span></span>
+                    </div>
+
+
+
+                    <div class="form-group">
+                        <label for="nama_anak" class="col-md-3 control-label">Nama</label>
+                        <div class="col-md-9">
+                            <input type="text" class="form-control" name="nama_anak" placeholder="Nama">
+                        </div>
+                    </div>
+
+                    <div class="form-group">
+                        <label for="tgl_lhr" class="col-md-3 control-label">Tempat/Tanggal Lahir</label>
+                        <div class="col-md-9">
+                            <input type="text" class="form-control" name="tgl_lhr" placeholder="Tempat/Tanggal Lahir">
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <label for="jk" class="col-md-3 control-label">Jenis Kelamin</label>
+                        <div class="col-md-9">
+                            <input type="text" class="form-control" name="jk" placeholder="Jenis Kelamin">
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <label for="alamat" class="col-md-3 control-label">Alamat</label>
+                        <div class="col-md-9">
+                            <input type="alamat" class="form-control" name="alamat" placeholder="Alamat">
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <label for="nama_ayah" class="col-md-3 control-label">Nama Ayah</label>
+                        <div class="col-md-9">
+                            <input type="nama_ayah" class="form-control" name="nama_ayah" placeholder="Nama Ayah">
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <label for="nama_ibu" class="col-md-3 control-label">Nama Ibu</label>
+                        <div class="col-md-9">
+                            <input type="nama_ibu" class="form-control" name="nama_ibu" placeholder="Nama Ibu">
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <label for="anakke" class="col-md-3 control-label">Anak Ke</label>
+                        <div class="col-md-9">
+                            <input type="anakke" class="form-control" name="anakke" placeholder="Anak Ke">
+                        </div>
+                    </div>
+
+                    <div style="float:right" class="form-group">
+                        <!-- Button -->
+
+                        <div class="col-sm-12 controls">
+                            <a id="btn-fblogin" href="#" class="btn btn-primary">Kirim</a>
+
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </div>
+
+
+
+
+    </div>
+</div>
+<br><br>
+
+@endsection
